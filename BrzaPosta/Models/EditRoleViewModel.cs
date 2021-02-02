d1b53e89607796245f677c9985dff92878537d77
Domagoj Perić <domagoj.peric@diplomat.ba>
2021-02-03 00:39:47 +0100

    Grammar Patch

--- BrzaPosta/Models/EditRoleViewModel.cs
+++ BrzaPosta/Models/EditRoleViewModel.cs
@@ -14,6 +14,7 @@ namespace BrzaPosta.Models
         }
         public string Id { get; set; }
         [Required]
+        [Display(Name = "Naziv")]
         public string Name { get; set; }
 
         public List<string> Users { get; set; }
